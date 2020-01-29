8672e505c2edfc43bd999dffdff43516d22880fa
leticiamrosa <leticia@vizir.com.br>
2020-01-29 14:31:38 -0300

    feat: implementation update user infos

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -16,6 +16,30 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExistis = await User.findOne({ where: { email } });
+
+      if (userExistis) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    const checkPassword = await user.checkPassword(oldPassword);
+
+    if (oldPassword && !checkPassword) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
